ea2169ea8e08debec20252caf7ddf6afb24bcd53
spvvrv <s.pivovarova@rambler.ru>
2023-02-09 17:28:22 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
+const splitString = (str, length) => str.match(new RegExp(`.{1,${length}}`, 'g'));
+
+const removeRedundantZeros = (str) => parseInt(str, 2).toString(2);
+
+const convertBinToMorse = (binaryStr) => splitString(binaryStr, 2).map(item => +item === 10 ? '.' : +item === 11 ? '-' : ' ').join('');
+
+const convertMorseToSymbol = (morseStr) => MORSE_TABLE[morseStr] || ' ';
+
 function decode(expr) {
-    // write your solution here
+    return splitString(expr, 10).
+        map(item => removeRedundantZeros(item)).
+        map(item => convertBinToMorse(item)).
+        map(item => convertMorseToSymbol(item)).join('');
 }
 
 module.exports = {
